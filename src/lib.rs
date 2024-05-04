dd1e08f790161e2adc54c9221c3dfd3a4dd4718c
GEORGEMUTALE <mutalegeorge367@gmail.com>
2024-05-04 20:35:02 +0200

    Hot handshake

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,21 @@
-pub fn actions(n: u8) -> Vec<&'static str> {
-    unimplemented!("What is the secret handshake for {n}?")
+pub fn actions(value: u8) -> Vec<&'static str> {
+    let reverse = value / 16 % 2 != 0;
+    let value = value % 16;
+    let mut result = Vec::<&'static str>::new();
+    if value & 0x1 == 0x1 {
+        result.push("wink");
+    }
+    if value & 0x2 == 0x2 {
+        result.push("double blink");
+    }
+    if value & 0x4 == 0x4 {
+        result.push("close your eyes");
+    }
+    if value & 0x8 == 0x8 {
+        result.push("jump");
+    }
+    if reverse {
+        result.reverse();
+    }
+    result
 }
